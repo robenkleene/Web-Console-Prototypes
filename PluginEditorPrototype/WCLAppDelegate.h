acbebc3037f07a989b329271f99cb0d9d4f0c104
Roben Kleene <robenk@gmail.com>
2014-03-01 15:37:00 -0500

    Moved Core Data Stack to Plugin View Controller and started laying out Plugin View Controller interface

--- PluginEditorPrototype/WCLAppDelegate.h
+++ PluginEditorPrototype/WCLAppDelegate.h
@@ -9,13 +9,5 @@
 #import <Cocoa/Cocoa.h>
 
 @interface WCLAppDelegate : NSObject <NSApplicationDelegate>
-
 @property (assign) IBOutlet NSWindow *window;
-
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-
-- (IBAction)saveAction:(id)sender;
-
 @end
