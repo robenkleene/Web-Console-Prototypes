72dbc228080b1bf10ff8c7660b3344b28b3a7a5b
Roben Kleene <robenk@gmail.com>
2014-03-02 12:45:41 -0500

    Clean up

--- PluginEditorPrototype/WCLEnvironmentViewController.m
+++ PluginEditorPrototype/WCLEnvironmentViewController.m
@@ -35,51 +35,6 @@ NSString * const WCLEnvironmentVariableValueKey = @"value";
     [self.tableView editColumn:0 row:[self.tableView selectedRow] withEvent:nil select:YES];
 }
 
-//- (IBAction)addEnvironmentVariable:(id)sender
-//{
-//    NSWindow *window = [self.tableView window];
-//    BOOL success = [window makeFirstResponder:window];
-//    if(!success) return; // First responder did not resign
-//
-//    NSMutableDictionary *environmentVariableDictionary = [self.arrayController newObject];
-//    
-//    environmentVariableDictionary[WCLEnvironmentVariableNameKey] = kEnvironmentVariableDefaultName;
-//    environmentVariableDictionary[WCLEnvironmentVariableValueKey] = kEnvironmentVariableDefaultValue;
-//
-//    [self.arrayController addObject:environmentVariableDictionary];
-//    
-//    NSUInteger rowIndex = [[self.arrayController arrangedObjects] indexOfObject:environmentVariableDictionary];
-//
-//    NSAssert(rowIndex != NSNotFound, @"The environment variable dictionary should be in the NSArrayController's arranged objects.");
-//
-////    [self.tableView selectRowIndexes:[NSIndexSet indexSetWithIndex:rowIndex] byExtendingSelection:NO];
-//    [self.tableView editColumn:0 row:rowIndex withEvent:nil select:YES];
-//}
-
-
-- (void)awakeFromNib
-{
-//    [_arrayController addObserver:self forKeyPath:@"selectionIndexes" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:NULL];
-
-//    [_dictionaryController addObserver:self forKeyPath:@"selectionIndexes" options:NSKeyValueObservingOptionNew context:NULL];
-}
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
-{
-
-
-    
-    if ([keyPath isEqualToString:@"selection"]) {
-//        id oldValue = [change objectForKey:NSKeyValueChangeOldKey];
-//        id newValue = [change objectForKey:NSKeyValueChangeNewKey];
-//        NSLog(@"oldValue = %@", oldValue);
-//        NSLog(@"newValue = %@", newValue);
-
-        NSLog(@"observeValueForKeyPath: [self.arrayController selectedObjects]  = %@", [self.dictionaryController selectedObjects]);
-        
-    }
-}
-
 - (NSDictionaryController *)dictionaryController
 {
     return _dictionaryController;
@@ -88,10 +43,9 @@ NSString * const WCLEnvironmentVariableValueKey = @"value";
 - (void)setDictionaryController:(NSDictionaryController *)dictionaryController
 {
     if (_dictionaryController != dictionaryController) {
+        [dictionaryController setInitialKey:kEnvironmentVariableDefaultKey];
+        [dictionaryController setInitialKey:kEnvironmentVariableDefaultValue];
         _dictionaryController = dictionaryController;
-        [_dictionaryController addObserver:self forKeyPath:@"selection" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:NULL];
-        [_dictionaryController setInitialKey:kEnvironmentVariableDefaultKey];
-        [_dictionaryController setInitialKey:kEnvironmentVariableDefaultValue];
     }
 }
 
