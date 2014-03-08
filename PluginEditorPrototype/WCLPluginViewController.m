288d740d9bd090cfc21adc5a22d56c1320fda6c1
Roben Kleene <robenk@gmail.com>
2014-03-08 16:01:19 -0500

    Make first responder solution

--- PluginEditorPrototype/WCLPluginViewController.m
+++ PluginEditorPrototype/WCLPluginViewController.m
@@ -1,3 +1,4 @@
+
 //
 //  WCLPluginViewController.m
 //  PluginEditorPrototype
@@ -9,22 +10,96 @@
 #import "WCLPluginViewController.h"
 #import "WCLPluginManager.h"
 
-@interface WCLPluginNameTextField : NSTextField
-@end
+#warning Remove with swizzling
+#import <objc/runtime.h>
 
 #warning Move this somewhere where it's accessible to whole app
 #define kAppName (NSString *)[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleName"]
 #define kPlugInExtension @"wcplugin"
 #define kPluginNameKey @"name"
 
-@implementation WCLPluginNameTextField
-- (void)mouseDown:(NSEvent *)theEvent {
-    // Intercept the mouse down event so the whole text field contents becomes selected instead of inserting a cursor.
+//@interface WCLTextFieldCell : NSTextFieldCell
+//@end
+//@implementation WCLTextFieldCell
+//
+//- (BOOL)resignFirstResponder
+//{
+//    NSLog(@"resign first responder");
+//    return [super resignFirstResponder];
+//}
+//@end
+
+@interface NSWindow (Swizzling)
+@end
+
+
+@implementation NSWindow (Swizzling)
+- (BOOL)replacementMakeFirstResponder:(NSResponder *)responder
+{
+    NSLog(@"responder is %@", responder);
+    return [self replacementMakeFirstResponder:responder];
+}
++ (void)load {
+    SEL originalSelector = @selector(makeFirstResponder:);
+    SEL overrideSelector = @selector(replacementMakeFirstResponder:);
+    Method originalMethod = class_getInstanceMethod(self, originalSelector);
+    Method overrideMethod = class_getInstanceMethod(self, overrideSelector);
+    if (class_addMethod(self, originalSelector, method_getImplementation(overrideMethod), method_getTypeEncoding(overrideMethod))) {
+        class_replaceMethod(self, overrideSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, overrideMethod);
+    }
 }
 @end
 
 
+@class WCLPluginNameTextField;
+
+@protocol WCLPluginNameTextFieldDelegate <NSTextFieldDelegate>
+@optional
+- (void)pluginNameTextFieldWillEndEditing:(WCLPluginNameTextField *)pluginNameTextField;
+@end
+
+@interface WCLPluginNameTextField : NSTextField
+- (id <WCLPluginNameTextFieldDelegate>)delegate;
+- (void)setDelegate:(id< NSTextFieldDelegate>)anObject;
+@end
+
+@implementation WCLPluginNameTextField
+- (void)mouseDown:(NSEvent *)theEvent {
+    // Intercept the mouse down event so the whole text field contents becomes selected instead of inserting a cursor.
+}
+- (id <WCLPluginNameTextFieldDelegate>)delegate
+{
+    return (id <WCLPluginNameTextFieldDelegate>)[super delegate];
+}
+- (void)setDelegate:(id <WCLPluginNameTextFieldDelegate>)anObject
+{
+    [super setDelegate:anObject];
+}
+- (BOOL)resignFirstResponder
+{
+    NSLog(@"resign first responder");
+    return [super resignFirstResponder];
+}
+- (BOOL)textShouldEndEditing:(NSText *)textObject
+{
+    if ([self.delegate respondsToSelector:@selector(pluginNameTextFieldWillEndEditing:)]) {
+        [self.delegate pluginNameTextFieldWillEndEditing:self];
+    }
+    return [super textShouldEndEditing:textObject];
+}
+@end
+
+
+@class WCLPluginArrayController;
+@protocol WCLPluginArrayControllerDelegate <NSObject>
+@optional
+- (void)pluginArrayControllerDidRearrangeObjects:(WCLPluginArrayController *)pluginArrayController;
+@end
+
 @interface WCLPluginArrayController : NSArrayController
+@property (nonatomic, weak) id delegate;
 @end
 
 @implementation WCLPluginArrayController
@@ -32,12 +107,37 @@
 {
     return [[WCLPluginManager sharedPluginManager] newPlugin];
 }
+
+- (void)rearrangeObjects
+{
+//    [super rearrangeObjects];
+//    return;
+    
+    NSLog(@"Before");
+    if ([self.delegate respondsToSelector:@selector(pluginArrayControllerDidRearrangeObjects:)]) {
+        [self.delegate pluginArrayControllerDidRearrangeObjects:self];
+    }
+
+    
+    NSArray *selectedObjects = [self selectedObjects];
+    [super rearrangeObjects];
+    [self setSelectedObjects:selectedObjects];
+    // Need to make the table view first responder?
+
+    NSLog(@"After");
+    if ([self.delegate respondsToSelector:@selector(pluginArrayControllerDidRearrangeObjects:)]) {
+        [self.delegate pluginArrayControllerDidRearrangeObjects:self];
+    }
+    
+}
 @end
 
 
-@interface WCLPluginViewController ()
+@interface WCLPluginViewController () <NSTableViewDelegate, NSTextFieldDelegate>
 @property (weak) IBOutlet WCLPluginArrayController *pluginArrayController;
 @property (weak) IBOutlet NSTableView *tableView;
+@property (assign) BOOL pendingMakeTableViewFirsResponder;
+- (void)makeTableViewFirstResponder;
 @end
 
 @implementation WCLPluginViewController
@@ -48,7 +148,7 @@
 {
     id newObject = [self.pluginArrayController newObject];
     [self.pluginArrayController addObject:newObject];
-    [self.pluginArrayController rearrangeObjects];
+//    [self.pluginArrayController rearrangeObjects];
 
     // Simple re-implement of NSDictionaryController add because using the add: method waits for the next run loop before updating the table view.
     [self.tableView editColumn:0 row:[self.tableView selectedRow] withEvent:nil select:YES];
@@ -60,6 +160,59 @@
     return YES;
 }
 
+- (void)awakeFromNib
+{
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowUpdated:) name:NSWindowDidUpdateNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+           selector:@selector(tableViewTextDidEndEditing:)
+               name:NSTextDidEndEditingNotification
+             object:self.tableView];
+}
+- (void)tableViewTextDidEndEditing:(NSNotification *)notification
+{
+    NSLog(@"break");
+}
+//
+//- (void)windowUpdated:(NSNotification *)notification
+//{
+//    NSLog(@"window updated [self.view.window firstResponder] = %@", [self.view.window firstResponder]);
+//
+//    if ([self.view.window firstResponder] == self.view.window) {
+//        NSLog(@"break");
+//    }
+//}
+
+- (void)controlTextDidEndEditing:(NSNotification *)obj {
+    NSLog(@"break");
+
+    [[[self view] window] performSelector:@selector(makeFirstResponder:) withObject:self.tableView afterDelay:0.0];
+}
+
+//- (void)controlTextDidChange:(NSNotification *)aNotification
+//{
+//    NSLog(@"break");
+//}
+//
+//- (void)controlTextDidBeginEditing:(NSNotification *)aNotification
+//{
+//    NSLog(@"Break");
+//}
+
+#pragma mark - WCLPluginNameTextFieldDelegate
+
+- (void)pluginNameTextFieldWillEndEditing:(WCLPluginNameTextField *)pluginNameTextField
+{
+    if (self.pendingMakeTableViewFirsResponder) return;
+    
+    self.pendingMakeTableViewFirsResponder = YES;
+    [self performSelector:@selector(makeTableViewFirstResponder) withObject:nil afterDelay:0.0];
+}
+
+- (void)makeTableViewFirstResponder
+{
+    [[[self view] window] makeFirstResponder:self.tableView];
+    self.pendingMakeTableViewFirsResponder = NO;
+}
 
 - (IBAction)removePlugin:(id)sender
 {
@@ -85,6 +238,36 @@
     [self.pluginArrayController remove:nil];
 }
 
+#pragma mark - WCLPluginArrayControllerDelegate
+
+
+- (void)pluginArrayControllerDidRearrangeObjects:(WCLPluginArrayController *)pluginArrayController
+{
+    // This just needs to be dispatched to the table view after editing occurs, probably from the cell itself.
+    NSLog(@"[self.view.window firstResponder] = %@", [self.view.window firstResponder]);
+//    [[[self view] window] performSelector:@selector(makeFirstResponder:) withObject:self.tableView afterDelay:0.0];
+
+    
+//    [[self.view window] makeFirstResponder:self];
+//    [[self.view window] makeFirstResponder:[self.tableView rowViewAtRow:[self.tableView selectedRow] makeIfNecessary:NO]];
+}
+
+//#pragma mark - NSTextFieldDelegate
+//
+//- (BOOL)control:(NSControl *)control textShouldEndEditing:(NSText *)fieldEditor
+//{
+//    return YES;
+//}
+
+#pragma mark - NSTableViewDelegate
+
+//- (void)tableView:(NSTableView *)tableView didRemoveRowView:(NSTableRowView *)rowView forRow:(NSInteger)row
+//{
+//    [[self.view window] makeFirstResponder:self.tableView];
+//    
+//    NSLog(@"break");
+//}
+
 #pragma mark - Properties
 
 - (WCLPluginArrayController *)pluginArrayController
@@ -96,6 +279,7 @@
 {
     if (_pluginArrayController == pluginArrayController) return;
     
+    pluginArrayController.delegate = self;
     NSSortDescriptor *nameSortDescriptor = [[NSSortDescriptor alloc] initWithKey:kPluginNameKey
                                                                        ascending:YES
                                                                         selector:@selector(localizedCaseInsensitiveCompare:)];
