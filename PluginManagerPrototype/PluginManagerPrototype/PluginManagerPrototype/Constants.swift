fdea218423f394bb8d28fffbcb34ea95f2499771
Roben Kleene <robenk@gmail.com>
2014-09-13 16:14:14 -0400

    Setup PluginManager in Swift

---
+++ PluginManagerPrototype/PluginManagerPrototype/PluginManagerPrototype/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  PluginManagerPrototype
+//
+//  Created by Roben Kleene on 9/13/14.
+//  Copyright (c) 2014 Roben Kleene. All rights reserved.
+//
+
+let pluginsFileExtension = "wcplugin"
