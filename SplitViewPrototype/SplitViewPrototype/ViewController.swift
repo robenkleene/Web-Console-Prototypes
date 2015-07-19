09688b55cb73efa22e35bf81b635b97ea7cd9bcd
Roben Kleene <robenk@gmail.com>
2015-07-19 16:09:08 -0400

    Add SpitViewPrototype

---
+++ SplitViewPrototype/SplitViewPrototype/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SplitViewPrototype
+//
+//  Created by Roben Kleene on 7/19/15.
+//  Copyright (c) 2015 Roben Kleene. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
