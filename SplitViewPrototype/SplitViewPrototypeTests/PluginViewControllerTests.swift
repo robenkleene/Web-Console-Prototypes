7b96ff2c64307a825fcb52899d0b508a4be8f355
Roben Kleene <robenk@gmail.com>
2015-08-02 14:54:44 -0400

    Fortify test code

--- SplitViewPrototype/SplitViewPrototypeTests/PluginViewControllerTests.swift
+++ SplitViewPrototype/SplitViewPrototypeTests/PluginViewControllerTests.swift
@@ -20,31 +20,39 @@ class PluginViewControllerTests: XCTestCase {
         PluginWindowsController.sharedInstance.openNewPluginWindow()
         let window = NSApplication.sharedApplication().windows.last as! NSWindow
         pluginViewController = window.contentViewController as! PluginViewController
-
+        NSUserDefaults.standardUserDefaults().removeObjectForKey(pluginViewController.logSplitViewSubviewSavedFrameName)
     }
     
     override func tearDown() {
+        NSUserDefaults.standardUserDefaults().removeObjectForKey(pluginViewController.logSplitViewSubviewSavedFrameName)
         pluginViewController = nil
         super.tearDown()
     }
     
     func testPluginViewController() {
+        // The log starts collapsed
         XCTAssertTrue(pluginViewController.logSplitViewItem.collapsed, "The  NSSplitViewItem should be collapsed")
+
+        // Show the log
         pluginViewController.toggleLogShown(nil)
         XCTAssertFalse(pluginViewController.logSplitViewItem.collapsed, "The  NSSplitViewItem should not be collapsed")
-
         XCTAssertEqual(logViewHeight, splitWebViewHeight, "The heights should be equal")
 
         let expectation = expectationWithDescription("NSUserDefaults did change")
-        NSNotificationCenter.defaultCenter().addObserverForName(NSUserDefaultsDidChangeNotification, object: nil, queue: nil, usingBlock: {
+        var observer: NSObjectProtocol?
+        observer = NSNotificationCenter.defaultCenter().addObserverForName(NSUserDefaultsDidChangeNotification, object: nil, queue: nil) {
             [unowned self] _ in
             expectation.fulfill()
-            NSLog("NSUserDefaultsDidChangeNotification")
-            })
+            if let observer = observer {
+                NSNotificationCenter.defaultCenter().removeObserver(observer)
+            }
+            observer = nil
+        }
 
         resizeLogViewHeight(testLogViewHeight)
         waitForExpectationsWithTimeout(testTimeout, handler: nil)
-                
+        XCTAssertEqual(logViewHeight, testLogViewHeight, "The heights should be equal")
+        
         // TODO: Wait for expectation with time out that the change gets saved?
         // TODO: Figure out how to resize the divider
         // TODO: Figure out whether the changed height is being saved to `NSUserDefaults`
